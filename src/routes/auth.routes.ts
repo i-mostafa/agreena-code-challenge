9d3dcc3a3940b7d20a53c6357258351467c6c29d
Islam Mostafa <islam.mostafa.fahmey@gmail.com>
2022-12-15 00:50:02 +0200

    Refactor Routes to add versioning

--- src/routes/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import { RequestHandler, Router } from "express";
-import { AuthController } from "modules/auth/auth.controller";
-
-const router = Router();
-const authController = new AuthController();
-
-router.post("/login", authController.login.bind(authController) as RequestHandler);
-
-export default router;
--- src/routes/user.routes.ts
+++
@@ -1,9 +0,0 @@
-import { RequestHandler, Router } from "express";
-import { UsersController } from "modules/users/users.controller";
-
-const router = Router();
-const usersController = new UsersController();
-
-router.post("/", usersController.create.bind(usersController) as RequestHandler);
-
-export default router;
