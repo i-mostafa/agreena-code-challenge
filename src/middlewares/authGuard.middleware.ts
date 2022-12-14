65b541b585ecb3f5d7bf7ad392707cae5cec9677
Islam Mostafa <islam.mostafa.fahmey@gmail.com>
2022-12-15 01:00:59 +0200

    Create Auth Guard middleware

---
+++ src/middlewares/authGuard.middleware.ts
@@ -0,0 +1,28 @@
+import { fromUnixTime } from "date-fns";
+import { NotAuthorizedError } from "errors/not-authorized.error";
+import { NextFunction, Request, Response } from "express";
+import { decode } from "jsonwebtoken";
+import { User } from "modules/users/entities/user.entity";
+
+export function authGuard(req: Request, _: Response, next: NextFunction): void {
+  const authorizationHeader = req.headers.authorization;
+  if (!authorizationHeader) return next(new NotAuthorizedError());
+
+  const token = authorizationHeader.split(" ")[1];
+  if (!token) return next(new NotAuthorizedError());
+
+  const decoded = decode(token);
+  if (!decoded) return next(new NotAuthorizedError());
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+  const { iat, exp, ...user } = decoded as Record<string, any>;
+
+  if (fromUnixTime(exp as number).getTime() < new Date().getTime()) return next(new NotAuthorizedError("Token Expired"));
+
+  // In a real application we should also search for this user in db
+  // as maybe this user token is valid but the user is deleted by an admin for example
+  // since we don't have this scenario here i will just assign it to request
+  req.user = user as User;
+
+  next();
+}
