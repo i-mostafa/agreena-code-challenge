c3dcb002002290bed7b431a286b3551db3b4cadf
Islam Mostafa <islam.mostafa.fahmey@gmail.com>
2022-12-15 00:59:26 +0200

    Update env Vars

--- src/config/environment-variables.ts
+++ src/config/environment-variables.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString, Max, Min } from "class-validator";
 
 export class EnvironmentVariables {
   @IsString()
@@ -37,11 +37,25 @@ export class EnvironmentVariables {
   @IsNotEmpty()
   public SALT_ROUNDS: number;
 
+  @IsNumber()
+  @IsNotEmpty()
+  public GOOGLE_MAPS_DISTANCE_MATRIX_LIMIT: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(0)
+  @Max(100)
+  public OUTLIERS_PERCENTAGE: number;
+
   @IsString()
   @IsNotEmpty()
   public JWT_SECRET: string;
 
-  @IsNumber()
+  @IsString()
   @IsNotEmpty()
-  public JWT_EXPIRES_AT: number;
+  public JWT_EXPIRES_AT: string;
+
+  @IsString()
+  @IsNotEmpty()
+  public GOOGLE_MAPS_API_KEY: string;
 }
